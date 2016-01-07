552493f2e3789cc9d9c32d848abcc6a9899dcdd0
Justin R. Wilson <jrw972@gmail.com>
2016-01-06 21:50:11 -0600

    Converage check
    Added coverage.sh script that does a coverage build.

---
+++ coverage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+find . -name "*.gcno" -delete
+find . -name "*.gcda" -delete
+rm -f utest_before.info utest_after.info utest.info
+rm -f ftest_before.info ftest_after.info ftest.info
+rm -rf html
+
+make clean
+CXXFLAGS="-O0 --coverage" ./configure
+make
+
+lcov --capture --initial --base-directory src --directory src --directory src/.libs --output-file utest_before.info --no-external
+pushd utest
+make check
+popd
+lcov --capture --base-directory src --directory src --directory src/.libs --output-file utest_after.info --no-external
+lcov --add-tracefile utest_before.info --add-tracefile utest_after.info --output-file utest.info --no-external
+genhtml -o html/utest utest.info
+
+lcov --capture --initial --base-directory src --directory src --directory src/.libs --output-file ftest_before.info --no-external
+pushd ftest
+make check
+popd
+lcov --capture --base-directory src --directory src --directory src/.libs --output-file ftest_after.info --no-external
+lcov --add-tracefile ftest_before.info --add-tracefile ftest_after.info --output-file ftest.info --no-external
+genhtml -o html/ftest ftest.info
--- precommit.sh
+++ precommit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 find src -name "*.[ch]pp" | xargs astyle --style=gnu --indent=spaces=2
-find test -name "*.[ch]pp" | xargs astyle --style=gnu --indent=spaces=2
+find utest -name "*.[ch]pp" | xargs astyle --style=gnu --indent=spaces=2
+find ftest -name "*.[ch]pp" | xargs astyle --style=gnu --indent=spaces=2
 
 echo -n '# of unimplemented code paths: '
 find src -name "*.[ch]pp" -o -name "*.[ly]" | xargs grep UNIMPLEMENTED | grep -v '#define' | wc -l
@@ -12,7 +13,7 @@ find src -name "*.[ch]pp" | xargs grep -o -h -E '\(E[0-9]+\)' | tr -d '()E' | so
 
 find src -name "*.[ch]pp" | xargs grep -o -h -E '\(E[0-9]+\)'  | tr -d '()E' | sort -n -u | sort > error_list
 
-grep -o -h -E 'E[0-9]+' test/*.sh | tr -d 'E' | sort -n -u | sort > tested_errors
+grep -o -h -E 'E[0-9]+' ftest/*.sh | tr -d 'E' | sort -n -u | sort > tested_errors
 
 echo "The following errors are not tested:"
 join -v 1 error_list tested_errors | sort -n | tr '\n' ' '
